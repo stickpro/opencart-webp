00c8e062fec53346d4dfeb4456a5b7a7ea998709
Vlad Bulgakov <stick.qwe@gmail.com>
2020-12-08 00:40:52 +0300

    remove tree

--- Vendor/frame_free/upload/catalog/language/en-gb/extension/module/framefreetheme/ff_cart.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Text
-$_['ff_heading_title']  = 'Shopping Сart';
-$_['ff_text_empty']     = 'Cart is empty!';
-$_['ff_text_cart']      = 'Go to Shopping Сart';
-$_['ff_text_checkout']  = 'Checkout';
-$_['ff_text_recurring'] = 'Payment profile';
-$_['ff_text_continue']  = 'Back';
--- Vendor/frame_free/upload/catalog/language/en-gb/extension/module/framefreetheme/ff_menu.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_category'] = 'Catalog';
-$_['text_all'] = 'Show All';
